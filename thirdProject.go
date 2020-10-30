aba7f33858d0fbce899ba5d78c022ed4f0b85bff
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-10-29 23:11:45 -0400

    add basic logic for using the drone's camera

--- thirdProject.go
+++ thirdProject.go
@@ -2,37 +2,70 @@ package main
 
 import (
 	"fmt"
-	// "fmt"
-	"time"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
+	"io"
+	"os/exec"
+	"time"
 )
 
-func main() {
-	drone := tello.NewDriver("8888")
+func setupMplayer() io.WriteCloser {
+	mplayer := exec.Command("mplyaer", "-fps", "25", "-")
 
-	work := func() {
-		err := drone.TakeOff()
-		if err != nil {
-			fmt.Println("Error making the drone take off %+v", err)
-		}
-		gobot.After(3*time.Second, func() {
-			err = drone.Left(50)
-			if err != nil {
-				fmt.Println("Error making the drone go left %+v", err)
-			}
-			time.Sleep(time.Second*3)
-			err = drone.Right(50)
-			if err != nil {
-				fmt.Println("Error making the drone go right %+v", err)
-			}
-			time.Sleep(time.Second*3)
-			err = drone.Land()
-			if err != nil {
-				fmt.Println("Error making the drone land %+v", err)
-			}
-		})
+	mplayerInput, err := mplayer.StdinPipe()
+	if err != nil {
+		fmt.Printf("Error creating input of mplayer: %+v\n", err)
 	}
+
+	err = mplayer.Start()
+	if err != nil {
+		fmt.Printf("Error starting mplayer: %+v\n", err)
+	}
+
+	return mplayerInput
+}
+
+func setupDroneVideo(drone *tello.Driver) {
+	err := drone.StartVideo()
+	if err != nil {
+		fmt.Printf("Error starting video: %+v\n", err)
+	}
+	err = drone.SetVideoEncoderRate(4)
+	if err != nil {
+		fmt.Printf("Error setting video encoder rate: %+v\n", err)
+	}
+	gobot.Every(100*time.Millisecond, func() {
+		err = drone.StartVideo()
+		if err != nil {
+			fmt.Printf("Error starting video: %+v\n", err)
+		}
+	})
+}
+
+func work(drone *tello.Driver) {
+	mplayerInput := setupMplayer()
+
+	err := drone.On(tello.ConnectedEvent, func(data interface{}) {
+		println("Connected")
+		setupDroneVideo(drone)
+	})
+	if err != nil {
+		fmt.Printf("Error setting ConnectedEvent event for drone: %+v\n", err)
+	}
+
+	err = drone.On(tello.VideoFrameEvent, func(data interface{}) {
+		packet := data.([]byte)
+		if _, err := mplayerInput.Write(packet); err != nil {
+			fmt.Printf("Error writing to mplayerInput: %+v\n", err)
+		}
+	})
+	if err != nil {
+		fmt.Printf("Error setting VideoFrameEvent event for drone: %+v\n", err)
+	}
+}
+
+func main() {
+	drone := tello.NewDriver("8890")
 
 	robot := gobot.NewRobot("Project 3: Drone",
 		[]gobot.Connection{},
@@ -41,8 +74,7 @@ func main() {
 	)
 
 	err := robot.Start()
-
 	if err != nil {
-		fmt.Errorf("Error starting the Drone %+v", err)
+		fmt.Printf("Error starting the Drone: %+v\n", err)
 	}
 }
