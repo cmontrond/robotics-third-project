d7c878b8d48eb3510b6a900bdea3386b01d06367
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-11-04 12:33:37 -0500

    clean code

--- thirdProject.go
+++ thirdProject.go
@@ -7,46 +7,28 @@ import (
 	"time"
 )
 
-func basic(drone *tello.Driver) {
-	// Functional Approach
-	TakeOff(drone)
+func basic(drone Drone) {
+	drone.TakeOff()
 	gobot.After(3*time.Second, func() {
-		Left(drone, 30)
-		time.Sleep(2 * time.Second)
-		Right(drone, 30)
-		time.Sleep(2 * time.Second)
-		Land(drone)
+		drone.Left(30)
+		SleepSeconds(2)
+		drone.Right(30)
+		SleepSeconds(2)
+		drone.Land()
 	})
-
-	// Type Approach
-	//myDrone := Drone{driver: driver}
-	//
-	//myDrone.TakeOff()
-	//gobot.After(3*time.Second, func() {
-	//	myDrone.Left(30)
-	//	SleepSeconds(2)
-	//	myDrone.Right(30)
-	//	SleepSeconds(2)
-	//	myDrone.Land()
-	//})
 }
 
-func work(drone *tello.Driver) {
-	// Functional approach
-	SetupCamera(drone, 4)
-
-	// Type approach
-	//myDrone := Drone{driver: driver}
-	//myDrone.SetupCamera(4)
+func work(drone Drone) {
+	drone.SetupCamera(4)
 }
 
 func main() {
 	driver := tello.NewDriver("8888")
 
-	//driver := Drone{driver: driver}
+	drone := Drone{driver: driver}
 
 	job := func() {
-		basic(driver)
+		basic(drone)
 	}
 
 	robot := gobot.NewRobot("Project 3: Drone",
