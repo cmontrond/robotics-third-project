aeb995351afe31d3d7505ded91624f9f0dc51a6c
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-11-12 20:45:24 -0500

    start drone and detect a face we need to make the image smoother / faster

--- thirdProject.go
+++ thirdProject.go
@@ -5,101 +5,117 @@ import (
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
 	"gocv.io/x/gocv"
+	"image/color"
 	"io"
+	"log"
 	"os/exec"
 	"strconv"
 	"time"
 )
 
+type pair struct {
+	x float64
+	y float64
+}
+
 const (
-	frameSize = 960 * 720 * 3
+	frameX    = 400
+	frameY    = 300
+	frameSize = frameX * frameY * 3
+	offset    = 32767.0
 )
 
-func setupFfmpeg(frameX int, frameY int) (*exec.Cmd, io.WriteCloser, io.ReadCloser) {
-
-	ffmpeg := exec.Command("ffmpeg", "-hwaccel", "auto", "-hwaccel_device", "opencl", "-i", "pipe:0",
+var (
+	// ffmpeg command to decode video stream from drone
+	ffmpeg = exec.Command("ffmpeg", "-hwaccel", "auto", "-hwaccel_device", "opencl", "-i", "pipe:0",
 		"-pix_fmt", "bgr24", "-s", strconv.Itoa(frameX)+"x"+strconv.Itoa(frameY), "-f", "rawvideo", "pipe:1")
+	ffmpegIn, _  = ffmpeg.StdinPipe()
+	ffmpegOut, _ = ffmpeg.StdoutPipe()
 
-	ffmpegIn, err := ffmpeg.StdinPipe()
+	// gocv
+	window     = gocv.NewWindow("Tello")
+	classifier *gocv.CascadeClassifier
+	green      = color.RGBA{G: 255}
 
-	if err != nil {
-		fmt.Printf("Error creating input of ffmpeg: %+v\n", err)
-	}
+	// drone
+	drone      = tello.NewDriver("8890")
+	flightData *tello.FlightData
+)
 
-	ffmpegOut, err := ffmpeg.StdoutPipe()
+func init() {
+	// process drone events in separate goroutine for concurrency
+	go func() {
 
-	if err != nil {
-		fmt.Printf("Error creating output of mplayer: %+v\n", err)
-	}
+		if err := ffmpeg.Start(); err != nil {
+			fmt.Println(err)
+			return
+		}
 
-	return ffmpeg, ffmpegIn, ffmpegOut
+		drone.On(tello.FlightDataEvent, func(data interface{}) {
+			// TODO: protect flight data from race condition
+			flightData = data.(*tello.FlightData)
+		})
+
+		drone.On(tello.ConnectedEvent, func(data interface{}) {
+			fmt.Println("Connected")
+			drone.StartVideo()
+			drone.SetVideoEncoderRate(tello.VideoBitRateAuto)
+			drone.SetExposure(0)
+			gobot.Every(100*time.Millisecond, func() {
+				drone.StartVideo()
+			})
+		})
+
+		drone.On(tello.VideoFrameEvent, func(data interface{}) {
+			pkt := data.([]byte)
+			if _, err := ffmpegIn.Write(pkt); err != nil {
+				fmt.Println(err)
+			}
+		})
+
+		robot := gobot.NewRobot("Project 3 - Drone",
+			[]gobot.Connection{},
+			[]gobot.Device{drone},
+		)
+
+		robot.Start()
+	}()
 }
 
-func basic(drone Drone) {
-	drone.TakeOff()
-	gobot.After(3*time.Second, func() {
-		drone.Left(30)
-		SleepSeconds(2)
-		drone.Right(30)
-		SleepSeconds(2)
-		drone.Land()
-	})
-}
+func trackFace(frame *gocv.Mat) {
 
-func work(drone Drone, window *gocv.Window, ffmpeg *exec.Cmd, ffmpegIn io.WriteCloser, ffmpegOut io.ReadCloser) {
-	//go func() {
-	//	drone.SetupCameraWithMplayer(4, 0)
-	//}()
+	imageRectangles := classifier.DetectMultiScale(*frame)
 
-	//go func() {
-	//	println("Started move drone goroutine!")
-	//	drone.TakeOff()
-	//	SleepSeconds(1)
-	//	drone.Hover()
-	//	SleepSeconds(3)
-	//	drone.Land()
-	//}()
+	for _, rect := range imageRectangles {
+		log.Println("found a face,", rect)
+		gocv.Rectangle(frame, rect, green, 3)
+	}
 }
 
 func main() {
-	driver := tello.NewDriver("8888")
-	window := gocv.NewWindow("Project 3")
 
-	drone := Drone{driver: driver}
+	cascadeClassifier := gocv.NewCascadeClassifier()
+	cascadeClassifier.Load("haarcascade_frontalface_default.xml")
 
-	ffmpeg, ffmpegIn, ffmpegOut := setupFfmpeg(960, 720)
-
-	job := func() {
-		//basic(drone)
-		work(drone, window, ffmpeg, ffmpegIn, ffmpegOut)
-	}
-
-	robot := gobot.NewRobot("Project 3: Drone",
-		[]gobot.Connection{},
-		[]gobot.Device{driver},
-		job,
-	)
-
-	err := robot.Start(false)
-	if err != nil {
-		fmt.Printf("Error starting the Drone: %+v\n", err)
-	}
-
-	drone.SetupCameraWithFfmpeg(window, ffmpeg, ffmpegIn, ffmpegOut, 30, 0, frameSize, 960, 720)
+	classifier = &cascadeClassifier
+	defer classifier.Close()
 
 	for {
+		// get next frame from stream
 		buf := make([]byte, frameSize)
 		if _, err := io.ReadFull(ffmpegOut, buf); err != nil {
 			fmt.Println(err)
 			continue
 		}
-		img, _ := gocv.NewMatFromBytes(720, 960, gocv.MatTypeCV8UC3, buf)
+		img, _ := gocv.NewMatFromBytes(frameY, frameX, gocv.MatTypeCV8UC3, buf)
 		if img.Empty() {
 			continue
 		}
 
+		trackFace(&img)
+
 		window.IMShow(img)
-		if window.WaitKey(1) >= 0 {
+		if window.WaitKey(10) >= 0 {
 			break
 		}
 	}
