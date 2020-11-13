a6c8479a020454fa9dc060dc307253506198be45
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-11-12 21:40:52 -0500

    add movement on different axis

--- thirdProject.go
+++ thirdProject.go
@@ -5,9 +5,10 @@ import (
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
 	"gocv.io/x/gocv"
+	"image"
 	"image/color"
 	"io"
-	"log"
+	"math"
 	"os/exec"
 	"strconv"
 	"time"
@@ -37,10 +38,22 @@ var (
 	classifier *gocv.CascadeClassifier
 	green      = color.RGBA{G: 255}
 
+	// tracking
+	tracking                 = false
+	detected                 = false
+	detectSize               = false
+	distTolerance            = 0.05 * dist(0, 0, frameX, frameY)
+	refDistance              float64
+	left, top, right, bottom int
+
 	// drone
 	drone      = tello.NewDriver("8890")
 	flightData *tello.FlightData
 )
+
+func dist(x1, y1, x2, y2 int) float64 {
+	return math.Sqrt(float64((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)))
+}
 
 func init() {
 	// process drone events in separate goroutine for concurrency
@@ -83,12 +96,73 @@ func init() {
 }
 
 func trackFace(frame *gocv.Mat) {
+	W := float64(frame.Cols())
+	H := float64(frame.Rows())
 
 	imageRectangles := classifier.DetectMultiScale(*frame)
 
 	for _, rect := range imageRectangles {
-		log.Println("found a face,", rect)
-		gocv.Rectangle(frame, rect, green, 3)
+		fmt.Printf("Found a face: %v\n", rect)
+
+		left = rect.Min.X
+		top = rect.Max.Y
+		right = rect.Max.X
+		bottom = rect.Min.Y
+
+		detected = true
+
+		face := image.Rect(left, top, right, bottom)
+		gocv.Rectangle(frame, face, green, 3)
+	}
+
+	if !tracking || !detected {
+		return
+	}
+
+	if detectSize {
+		detectSize = false
+		refDistance = dist(left, top, right, bottom)
+	}
+
+	distance := dist(left, top, right, bottom)
+
+	// x axis
+	switch {
+	case float64(right) < W/2:
+		//drone.CounterClockwise(50)
+		println("Drone moving counter clockwise...")
+	case float64(left) > W/2:
+		//drone.Clockwise(50)
+		println("Drone moving clockwise")
+	default:
+		//drone.Clockwise(0)
+		println("Drone not moving clockwise")
+	}
+
+	// y axis
+	switch {
+	case float64(top) < H/10:
+		//drone.Up(25)
+		println("Drone moving up...")
+	case float64(bottom) > H-H/10:
+		//drone.Down(25)
+		println("Drone moving Down...")
+	default:
+		//drone.Up(0)
+		println("Drone not moving up or down...")
+	}
+
+	// z axis
+	switch {
+	case distance < refDistance-distTolerance:
+		//drone.Forward(20)
+		println("Drone should move forward...")
+	case distance > refDistance+distTolerance:
+		//drone.Backward(20)
+		println("Drone should move backward...")
+	default:
+		//drone.Forward(0)
+		println("Drone should not move forward...")
 	}
 }
 
